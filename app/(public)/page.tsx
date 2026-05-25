c71b08b0c13cbaad8f3364f00838e8f3d891217a
Azebo-bet <azebobet@gmail.com>
2026-05-25 10:48:52 +0000

    feat: add card borders and gaps between sidebar, match list, and bet slip

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -26,7 +26,7 @@ export default async function HomePage() {
       <div className="sticky top-0 z-50">
         <PublicNavbar />
       </div>
-      <div className="flex flex-col mx-2 mt-2 rounded-xl overflow-hidden border border-nile-blue/20" style={{ height: 'calc(100vh - 70px)' }}>
+      <div className="flex flex-col mx-2 mt-2" style={{ height: 'calc(100vh - 70px)' }}>
         <MatchListClient
           initialMatches={initialMatches}
           countries={countries}
