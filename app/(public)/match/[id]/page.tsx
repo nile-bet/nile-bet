f77ed31e7b80b56f5338d91394afef11b2fb60ad
nile-bet <nilebetting@gmail.com>
2026-06-04 09:48:30 +0000

    fix: insert change password modal before confirm modal

--- app/(public)/match/[id]/page.tsx
+++ app/(public)/match/[id]/page.tsx
@@ -30,8 +30,8 @@ export default async function MatchPage({
   return (
     <div className="flex flex-col h-screen overflow-hidden">
       <PublicNavbar />
-      <div className="flex flex-1 overflow-hidden">
-        <div className="flex-1 overflow-y-auto">
+      <div className="flex flex-1 min-h-0 overflow-hidden">
+        <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
           <MatchDetailClient match={match} />
         </div>
         <BetSlipSidebar settings={settings} />
